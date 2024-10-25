3181a71094f33d94529d633868d8c9853eb232b7
sayantan roy <itsroysayantan@gmail.com>
2024-10-25 18:34:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// script.js
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('The page has fully loaded');
-    
-    // Set current year in footer
-    const currentYearSpan = document.getElementById('current-year');
-    if (currentYearSpan) {
-        currentYearSpan.textContent = new Date().getFullYear();
-    }
-});
+
